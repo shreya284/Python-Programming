c78ce376619e913216b78d1034b53248a3e7fd59
shreya284 <71548879+shreya284@users.noreply.github.com>
2021-08-19 13:56:33 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,14 @@
+#define a function sumOfSeries for input n
+def sumOfSeries(n):
+  sum=0
+  
+ #iteration loop to square each number and add them to get the square
+ for i in range(1, n+1):
+    sum = sum + (i*i)
+    
+    #returns the resulting square of a number input
+    return num
+  
+  #sample input
+  n=2
+  print(sumOfSeries(n))
