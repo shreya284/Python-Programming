2234ef4ac9572021fc03a2a55a083f293cf68c59
shreya284 <71548879+shreya284@users.noreply.github.com>
2021-08-19 14:03:20 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,9 @@
+#define the main function findArea for radius r to find the circle's area
+def findArea(r):
+    PI = 3.142
+    
+    #calculates the area of the circle by taking the value of r(radius)
+    return PI*r*r
+  
+  #prints the result of the area of the circle
+  print("Area is: %.6f", findArea(n));
